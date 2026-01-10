c9ea83d7f3d3247ef89b9f59841c11eda41ffed1
SAURABHSHINDE1 <saurabhshinde779845@gmail.com>
2026-01-10 17:44:56 +0530

    10 jan 2026

---
+++ basics/script.js
@@ -0,0 +1,74 @@
+// variables 
+// var
+// let
+// const
+
+// var a = 10
+
+// let b = 56
+
+// const c = 78
+
+// console.log(a)
+
+// console.log(b)
+
+// console.log(c)
+
+// var
+//1. it allows Reinitializtion tya navane same varibale tayar karne 
+//2. it allow reassign tya same varibale la navin value den
+//3. var ha function scoped variable asto
+
+// var a = "abc"
+
+// console.log(a)
+
+//  a = "xyz"
+
+// console.log(a)
+
+// var a = 10
+
+// function add(){
+
+    
+
+//     console.log(a)
+
+// }
+
+// add()
+
+// console.log(a)
+
+
+// let
+// let madhe aapn same name ne navin variable tayar karuu shakat nahhi // reinitializtion hou shakat nahii 
+// it allows re assign 
+// let ha ek block scoped varibale ahe 
+
+// let a = 10
+
+// console.log(a)
+
+// a = 78
+
+// console.log(a)
+
+
+
+// const
+// 1. same navane varibale tauar karu shakat nahiii
+// 2. const variable chi value aapn ekada declare kelaynanter program madhe change karu shakat nahiiii / constant value aste.
+// 3.const la reinitialization kiva reassign hou  shakat nahiiii
+// 4 . const ha block scoped varibale ahe .
+
+// const a = 46
+
+// console.log(a)
+
+//  a = 45
+
+//  console.log(a)
+
